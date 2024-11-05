d271f561cc4f4dd1acf0f9f8dba03c19428100b7
ifedayo <ifedayyo12@gmail.com>
2024-11-05 15:18:18 +0100

    push

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,8 +1,22 @@
+import "./App.css";
+import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <div>
-      <p> Life is very interesting</p>
-      <h1> Boots and Shoes</h1>
+      <>
+        <p> Bored? No more. Get those neurons firing with Quizzit </p>
+        <img
+          className="planetearth"
+          src="src/assets/planet_earth.webp"
+          alt="picture of our lovely planet earth"
+        />
+      </>
+
+      <div>
+        <Link to="/signup">
+          <button className="descriptionbutton">Get Started</button>
+        </Link>{" "}
+      </div>
     </div>
   );
 }
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,3 +1,44 @@
 export default function Signup() {
-  return <div></div>;
+  return (
+    <div>
+      We are all humans, and sometimes we want to feel our brain cells firing
+      rapidly shooting those damn guns, causing chaos and commotioon, scattering
+      everywhere. Interestingly there is no bullets around. but the neurons keep
+      firringgg. hahahahaha! I want to fire you like a neuron, just without
+      bullet. hahahahaha. Don't mind the craziness above. Fire away, and sign up
+      for our quizzes so you can get those neurons workkinggg. hahahahaha return
+      (
+      <>
+        <form className="form-styling">
+          <input
+            type="text"
+            id="fname"
+            className="input-styling"
+            name="fname"
+            placeholder="First name:"
+            required
+          />
+
+          <input
+            type="text"
+            id="lname"
+            className="input-styling"
+            name="lname"
+            placeholder="Last name"
+            required
+          />
+
+          <input
+            type="email"
+            id="email"
+            className="input-styling"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
+          <button className="button-styling">SUBMIT</button>
+        </form>
+      </>
+    </div>
+  );
 }
